14e9df6a5a0a46f89bf1c872c95aeac5bf99babd
NitinDev093 <nitinsahani093@gmail.com>
2025-11-07 12:01:10 +0530

    Added Forms Validation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -60,7 +60,7 @@ namespace childmanagementsystem.Controllers
         }
 
         //Get
-        public ActionResult SignUp(UserData_Model udm)
+        public ActionResult SignUp()
         {
             return View();
         }
